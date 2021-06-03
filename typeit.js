28cd5246c1ceeba1dbef73c37910e4147584fe81
Lucas Burlingham <lucas.burlingham@icloud.com>
2021-06-02 23:31:25 -0400

    Typeit JS configuration

---
+++ typeit.js
@@ -0,0 +1,20 @@
+new TypeIt('#welcome_typeit', {
+    strings: [
+        '<span class="bold">Welcome</span> <abbr style="border-bottom: 3px dotted; color:#fdb913">Students</abbr>!',
+        '<span class="bold">Welcome</span> <abbr style="border-bottom: 3px dotted; color:#fdb913">Parents</abbr>!',
+        '<span class="bold">Welcome</span> <abbr style="border-bottom: 3px dotted; color:#fdb913">Teachers</abbr>!',
+        '<span class="bold">Welcome</span> <abbr style="border-bottom: 3px dotted; color:#fdb913">Students</abbr>!'
+    ],
+    cursor: true,
+    cursorSpeed: 1000,
+    deleteSpeed: 50,
+    breakLines: false,
+    breakDelay: 750,
+    startDelay: 250,
+    startDelete: true,
+    nextStringDelay: 750,
+    loop: false,
+    html: true,
+    waitUntilVisible: false,
+    lifelike: true,
+});
